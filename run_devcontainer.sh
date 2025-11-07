03ba665188cbcd434dffd532e080993a5ff390a0
Archan Paul <archan.paul@gmail.com>
2025-11-07 18:09:09 +0530

    Cleanup folder for reuse.

--- run_devcontainer.sh
+++ run_devcontainer.sh
@@ -1,2 +1,3 @@
 podman run -d --name ml_devcontainer -v $(pwd):/home/vscode/workspace:Z localhost/ml_devcontainer:latest
+podman exec -it --user root ml_devcontainer bash -c "rm -rf /home/vscode/.vscode-server/*"
 podman exec -it --user root  ml_devcontainer bash

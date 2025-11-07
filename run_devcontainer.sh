2a0b996a1f5306ad82102c1f73b7171d5aba17a6
Archan Paul <archan.paul@gmail.com>
2025-11-07 19:09:27 +0530

    Permission related updates added.

--- run_devcontainer.sh
+++ run_devcontainer.sh
@@ -1,3 +1,3 @@
-podman run -d --name ml_devcontainer -v $(pwd):/home/vscode/workspace:Z localhost/ml_devcontainer:latest
+podman run -d --user $(id -u):$(id -g) --name ml_devcontainer -v $(pwd):/home/vscode/workspace:Z localhost/ml_devcontainer:latest
 podman exec -it --user root ml_devcontainer bash -c "rm -rf /home/vscode/.vscode-server/*"
 podman exec -it --user root  ml_devcontainer bash
